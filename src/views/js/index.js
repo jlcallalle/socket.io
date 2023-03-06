4bebbe4f628dc9e5ce7aef1c9005b7a56ceeee3e
Jorge Callalle <jorge.callalle@vasslatam.com>
2023-03-06 10:09:47 -0500

    switch server local

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,5 +1,5 @@
-const socket = io();
-// const socket = io("http://localhost:3000/", { forceNew: true });
+// const socket = io();
+const socket = io("http://localhost:3000/", { forceNew: true });
 const contenedor = document.getElementById('contenedor');
 
 function checkSocketStatus() {
