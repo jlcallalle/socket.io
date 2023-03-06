1af8528f398cb6cc880bd4926865dd289be326ce
Jorge Callalle <jorge.callalle@vasslatam.com>
2023-03-06 08:27:53 -0500

    switch server local

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,5 +1,5 @@
-const socket = io();
-// const socket = io("http://localhost:3000/", { forceNew: true })
+// const socket = io();
+const socket = io("http://localhost:3000/", { forceNew: true })
 
 socket.on("welcome", data => {
     const text = document.querySelector("#text");
