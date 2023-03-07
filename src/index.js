a25d3573922e1ba739e5a027b76133786ee34c7a
Jorge Callalle <jorge.callalle@vasslatam.com>
2023-03-06 21:03:44 -0500

    se agrega admin-ui

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 //process.env.DEBUG = "*";
-process.env.DEBUG = "engine, socket.io:socket, socket.io:client";
+// process.env.DEBUG = "engine, socket.io:socket, socket.io:client";
 const express = require("express");
 const path = require("path");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
 
 const app = express();
 const httpServer = createServer(app);
 // const io = new Server(httpServer);
 const io = new Server(httpServer, {
     cors: {
-        origin: ["https://invex.modyo.be/", "https://c7a3-179-6-171-28.ngrok.io"],
-      }
+        // origin: ["https://invex.modyo.be/", "https://c7a3-179-6-171-28.ngrok.io"],
+        origin: ["https://admin.socket.io", "https://invex.modyo.be/", "https://socket-io-jlcallalle.vercel.app/"],
+        credentials: true
+    }
 });
+
+// habilitar usar admin.ui
+/* instrument(io, {
+    auth: false
+}); */
+
+instrument(io, {
+    auth: {
+        type: "basic",
+        username: "admin",
+        password: "$2a$04$jOtX8wrr6MPA1xmHir4rWeFTt9hZWNIPwIXDichFyTD0nKbI1xTz6"
+    }
+});
+
 app.use( express.static(path.join(__dirname, "views")) );
 
 app.get("/", (req, res) => {
@@ -23,7 +40,7 @@ io.on("connection", socket => {
 
     // Información Servidor
     console.log('websocket Server');
-    console.log('socket handshake', socket.handshake);
+    // console.log('socket handshake', socket.handshake);
     console.log('socket id', socket.id);
     console.log('Clientes conectados', io.engine.clientsCount);
     /* socket.on ("disconnect", () => {
